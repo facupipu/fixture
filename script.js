213c93a1c71905619665ede736220437a06fa6d1
facupipu <102254049+facupipu@users.noreply.github.com>
2022-06-27 01:10:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// const MATCHES = {
+//   group1: [
+//     {
+//       home: "Argentina",
+//       visitor: "Arabia Saudita"
+//     },
+//     {
+//       home: "México",
+//       visitor: "Polonia"
+//     }
+//   ],
+//   group2: [
+
+//   ]
+// }
+
+// const matches = document.querySelectorAll('.group-matches')
+
+// matches.forEach(match => {
+//   const inputs = match.querySelectorAll('input[type="number"]')
+  
+//   inputs.forEach(input => {
+//     input.addEventListener('focusout', (event) => {
+//       console.log(input.value)
+//     })
+//   })
+
+  
+// })
